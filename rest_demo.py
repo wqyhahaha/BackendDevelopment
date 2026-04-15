d491148e5175b5f1ab3e2068e83be9c5a6ed863e
WQY <1803324225@qq.com>
2026-04-15 15:28:15 +0800

    first commit

---
+++ rest_demo.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app=FastAPI()
+
+users = [
+    {"id": 1, "name": "Alice", "age": 23},
+    {"id": 2, "name": "Bob", "age": 25}
+]
+
+
+class User(BaseModel):
+    name:str
+    age:int
+    
+@app.get("/users")
+def get_users():
+    return users
+
+@app.get("/users/{user_id}")
+def get_user(user_id:int):
+    for user in users:
+        if user['id']==user_id:
+            return user
+    return {"error":"user not found"}
+
+@app.post("/users")
+def create_user(user: User):
+    new_user={
+        "id":len(users)+1,
+        "name":user.name,
+        "age":user.age
+    }
+    users.append(new_user)
+    
+@app.delete("/users/{user_id}")
+def delete_user(user_id:int):
+    for user in users:
+        if user["id"]==user_id:
+            users.remove(user)
+            return {"message":"deleted"}
+    return {"error":"user not found"}
---
+++ test_api.py
@@ -0,0 +1,7 @@
+import requests
+
+url = "http://127.0.0.1:8000/users"
+
+response = requests.get(url)
+
+print(response.json())
